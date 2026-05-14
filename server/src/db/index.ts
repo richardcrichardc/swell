862df56fbce45892dda8855b5ac98554c9b0f9bf
Richard Collins <richardc@richardc.net>
2026-05-14 15:48:18 +1200

    Run database migrations on deploy

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,6 +1,9 @@
 import Database from 'better-sqlite3'
 import { drizzle } from 'drizzle-orm/better-sqlite3'
+import { migrate } from 'drizzle-orm/better-sqlite3/migrator'
 import { mkdirSync } from 'fs'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
 import * as schema from './schema'
 
 mkdirSync('./server/data', { recursive: true })
@@ -11,3 +14,6 @@ sqlite.pragma('journal_mode = WAL')
 sqlite.pragma('foreign_keys = ON')
 
 export const db = drizzle(sqlite, { schema })
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+migrate(db, { migrationsFolder: join(__dirname, '../../drizzle') })
